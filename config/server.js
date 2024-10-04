a56d6dc3565a402032fe9f6ad4a02ac2f901f574
root <root@e2e-101-241.ssdcloudindia.net>
2024-10-04 19:41:25 +0530

    added changes

--- config/server.js
+++ config/server.js
@@ -1,8 +1,13 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: 'https://store.dhairyawanhsc.com/dash/',
+  debug : true,
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  logger: {
+    level: 'debug',
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
