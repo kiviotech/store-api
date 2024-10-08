cabf38a420f06a9722ea84c5b15e2a0d5c5befb9
VipulMca <129883298+VipulMca@users.noreply.github.com>
2024-10-08 12:28:32 +0530

    order-history

--- src/api/order-detail/routes/order-detail.js
+++ src/api/order-detail/routes/order-detail.js
@@ -6,29 +6,29 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-// module.exports = createCoreRouter('api::order-detail.order-detail');
+module.exports = createCoreRouter('api::order-detail.order-detail');
 
-module.exports = {
-  routes: [
-    // Keep the existing routes
-    {
-      method: 'POST',
-      path: '/order-details',
-      handler: 'order-detail.create',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    // Add this new route
-    {
-      method: 'PUT',
-      path: '/order-details/:id/payment',
-      handler: 'order-detail.updatePayment',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-  ],
-};
+// module.exports = {
+//   routes: [
+//     // Keep the existing routes
+//     {
+//       method: 'POST',
+//       path: '/order-details',
+//       handler: 'order-detail.create',
+//       config: {
+//         policies: [],
+//         middlewares: [],
+//       },
+//     },
+//     // Add this new route
+//     {
+//       method: 'PUT',
+//       path: '/order-details/:id/payment',
+//       handler: 'order-detail.updatePayment',
+//       config: {
+//         policies: [],
+//         middlewares: [],
+//       },
+//     },
+//   ],
+// };
