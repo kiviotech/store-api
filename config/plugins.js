8ac1d2b5d7a1fb370d14b22d3006c84a432faffc
VipulMca <129883298+VipulMca@users.noreply.github.com>
2024-11-16 18:46:18 +0530

    bug fixes

--- config/plugins.js
+++ config/plugins.js
@@ -1,34 +1,36 @@
-module.exports = () => ({
+module.exports = ({ env }) => ({
   email: {
     config: {
       provider: "@strapi/provider-email-nodemailer",
       providerOptions: {
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
+        host: env("SMTP_HOST"),
+        port: env("SMTP_PORT"),
         auth: {
-          user: process.env.SMTP_USERNAME,
-          pass: process.env.SMTP_PASSWORD,
+          user: env("SMTP_USERNAME"),
+          pass: env("SMTP_PASSWORD"),
         },
       },
       settings: {
-        defaultFrom: process.env.SMTP_FROM,
-        defaultReplyTo: process.env.SMTP_REPLY_TO,
+        defaultFrom: env("SMTP_FROM"),
+        defaultReplyTo: env("SMTP_REPLY_TO"),
       },
     },
   },
   upload: {
     config: {
-      sizeLimit: 2500 * 1024 * 1024, // 256 MB in bytes
+      sizeLimit: 256 * 1024 * 1024, // 256 MB in bytes
     },
-  },
-  bootstrap({ strapi }) {
-    strapi.server.httpServer.requestTimeout = 30 * 60 * 1000; // Set request timeout to 30 minutes
   },
   "users-permissions": {
     config: {
       jwt: {
-        expiresIn: "7d",
+        expiresIn: "7d", // Set token expiration to 1 minute for testing
       },
     },
   },
 });
+
+// Set the server request timeout
+module.exports.bootstrap = ({ strapi }) => {
+  strapi.server.httpServer.requestTimeout = 30 * 60 * 1000; // 30 minutes
+};
