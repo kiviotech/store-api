888650797d7de9d3ba624567bf1eee04e8e815fb
vipul <sharma.vipulmca@gmail.com>
2025-03-19 12:04:17 +0530

    UI chnages

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,4 +1,3 @@
-// config/middlewares.js
 module.exports = ({ env }) => [
   'strapi::logger',
   'strapi::errors',
@@ -6,18 +5,25 @@ module.exports = ({ env }) => [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['https://store.dhairyawanhsc.com/','http://localhost:8082','*'],
+      origin: [
+        'https://store.dhairyawanhsc.com', // Production frontend
+        'http://localhost:1337', // Strapi backend (local)
+        'http://localhost:8081', // React Native Metro Bundler
+        'http://192.168.0.102:1337', // Local network access
+        'http://192.168.0.102:8081' // React Native on local network
+      ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      credentials: true // Allow cookies and auth headers
     },
   },
   {
     name: 'strapi::body',
     config: {
-      formLimit: '2gb',  // Set form data size limit
-      jsonLimit: '2gb',  // Set JSON payload size limit
-      textLimit: '2gb',  // Set text payload size limit
+      formLimit: '2gb',  
+      jsonLimit: '2gb',  
+      textLimit: '2gb',  
       formidable: {
-        maxFileSize: 2 * 1024 * 1024 * 1024 // Set max file size to 2GB
+        maxFileSize: 2 * 1024 * 1024 * 1024 
       }
     },
   },
